46e54edf547acb18399e6f7d157b29801cfc3a76
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2022-05-02 19:43:45 +0300

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField('Лошара', max_length=50)
+    task = models.TextField('СуперЛох', max_length=50)
+
+    def __str__(self):
+        return self.title
+    class Meta():
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачi"
